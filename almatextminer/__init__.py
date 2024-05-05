d508affd47611c60decd7a759184d80de7a4cd1e
Noah McIntire <ne19mc@gmail.com>
2024-05-05 17:02:13 -0400

    add base structure

---
+++ almatextminer/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+# This file tells Python that this directory should be treated as a package.
+
+# Import any modules or subpackages that you want to expose when the package is imported.
+import pandas as pd
+import numpy as np  #for text pre-processing
+import re, string
+from joblib import dump, load
---
+++ almatextminer/almatextminer.py
@@ -0,0 +1,24 @@
+# module_name.py
+
+# Define your functions, classes, or variables here.
+class AlmaTextMiner:
+
+    def __init__(self):
+        self.LinearClassifier = load('path_to/linear_classifer')
+        self.LDA = load('path_to/lDA')
+        self.topic_cluster = pd.read_csv('path_to/topic_cluster_table')
+        self.topic_measurement = pd.read_csv('path_to/topic_measurement_table')
+        self.Band_Classifier = load('path_to/classifier')
+        self.tfidf_vectorizers
+
+
+
+
+    def build_recommendation(self, title, abstract):
+
+
+
+    def _preprocess(self, text):
+
+        
+    
